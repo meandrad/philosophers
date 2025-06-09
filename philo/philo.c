96af338dac6d3bd38bf8524aa05db1fb7c9c4bad
Melissa Andrade Garcia Da Silva <meandrad@c1r9p1.42sp.org.br>
2025-06-09 19:54:40 -0300

    adicionando main

--- philo/philo.c
+++ philo/philo.c
@@ -6,9 +6,21 @@
 /*   By: meandrad <meandrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 21:49:41 by meandrad          #+#    #+#             */
-/*   Updated: 2025/05/27 19:30:34 by meandrad         ###   ########.fr       */
+/*   Updated: 2025/06/09 19:40:07 by meandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int	main(int argc, char *argv[])
+{
+	if (argc == 5)
+	{
+	}
+	else if (argc == 6)
+	{
+	}
+	else
+		ft_putstr(ERROR_MESSAGE, 2);
+	return (0);
+}
